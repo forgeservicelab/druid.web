ad61c49bba23f7a6913ab45aeaa369812feb9fb6
Ari Ruuska <ruuska.ari@gmail.com>
2014-03-24 16:55:26 +0200

    Added Math.round to calculated field

--- sites/all/modules/features/forge_web_wizard/forge_web_wizard.js
+++ sites/all/modules/features/forge_web_wizard/forge_web_wizard.js
@@ -9,7 +9,7 @@ Drupal.behaviors.myBehavior = {
   attach: function (context, settings) {
     $("#edit-submitted-other-contract-information-the-value-of-development-project").blur(function() {
       val = parseInt($("#edit-submitted-other-contract-information-the-value-of-development-project").val());
-      calculated_value = val*0.05;
+      calculated_value = Math.round(val*0.05);
       $("#edit-submitted-other-contract-information-digile-service-fee").val(calculated_value);
     });
   }
