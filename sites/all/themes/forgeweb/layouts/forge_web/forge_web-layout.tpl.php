2a50fc8ccee7f78a6cc57a9fc62cae79025c21d1
Tero Elonen <tero.elonen@druid.fi>
2014-04-09 22:54:07 +0300

    styled general items and layouts for plaza layout

--- sites/all/themes/forgeweb/layouts/forge_web/forge_web-layout.tpl.php
+++ sites/all/themes/forgeweb/layouts/forge_web/forge_web-layout.tpl.php
@@ -1,53 +1,42 @@
 <div<?php print $attributes; ?>>
   <header class="l-header" role="banner">
-    <?php print render($page['header']); ?>
-    
-    <div class="l-branding">
-      <?php if ($logo): ?>
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+    <div class="l-navigation-wrapper">
+      <?php if ($site_name): ?>
+        <a href="<?php print $front_page; ?>" class="home-icon" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
       <?php endif; ?>
-
-      <?php if ($site_name || $site_slogan): ?>
-        <?php if ($site_name): ?>
-          <h1 class="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-          </h1>
-        <?php endif; ?>
-
-        <?php if ($site_slogan): ?>
-          <h2 class="site-slogan"><?php print $site_slogan; ?></h2>
-        <?php endif; ?>
-      <?php endif; ?>
-
-      <?php print render($page['branding']); ?>
+      
+      <?php print render($page['navigation']); ?>
     </div>
-    <?php print render($page['navigation']); ?>
   </header>
-
-  <div class="l-main">
-    <div class="l-content" role="main">
+  
+  <?php if ($page['highlighted']): ?>
+    <div class="l-highlighted">
       <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
+    </div>
+  <?php endif; ?>
+  
+  <div class="l-main">
+    <div class="l-content " role="main">
       <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
       <?php print $messages; ?>
       <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
       <?php if ($action_links): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
       <?php print render($page['content']); ?>
       <?php print $feed_icons; ?>
     </div>
-
+    
     <?php print render($page['sidebar_first']); ?>
     <?php print render($page['sidebar_second']); ?>
   </div>
-
+  
+  <?php if ($page['after_content']): ?>
+    <div class="l-after_content">
+      <?php print render($page['after_content']); ?>
+    </div>
+  <?php endif; ?>
+  
   <footer class="l-footer" role="contentinfo">
     <div class="l-footer-wrapper">
       <?php print render($page['footer_left']); ?>
