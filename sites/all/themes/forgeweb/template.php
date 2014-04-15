3377cf238035190b02f7e6c0e7ec0981a4d4e950
Tero Elonen <tero.elonen@druid.fi>
2014-04-16 00:18:53 +0300

    service offering page changes and styling, company page changes and styling

--- sites/all/themes/forgeweb/template.php
+++ sites/all/themes/forgeweb/template.php
@@ -5,3 +5,17 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * forgeweb theme.
  */
+
+ // Alter the default comment form
+ function forgeweb_form_comment_form_alter(&$form, &$form_state) {
+   $form['comment_body']['#after_build'][] = 'forgeweb_customize_comment_form';
+ }
+
+ function forgeweb_customize_comment_form(&$form) {
+   // Force comment text-format
+   $form[LANGUAGE_NONE][0]['format']['format']['#value'] = 'comment';
+   
+   // Disable the text-format change from users on comments-
+   $form[LANGUAGE_NONE][0]['format']['#access'] = FALSE;
+   return $form;
+ }
