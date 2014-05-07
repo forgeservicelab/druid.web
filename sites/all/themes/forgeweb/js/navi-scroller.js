ef5bc94e59742998a377a69f1c7b576b54f49c20
ernokoli <erno@ernokoliseva.net>
2014-05-07 15:49:06 +0300

    Fixed broken paths, added js-file for navi-scroller

---
+++ sites/all/themes/forgeweb/js/navi-scroller.js
@@ -0,0 +1,10 @@
+// Handles scrolling from submenu items
+(function ($) {
+  $(function () {
+    $('#menu-block-menu-1 #rm-no-id').click {
+      $('html, body').animate({
+        scrollTop: $node.offset().top
+      }, 2000);
+    });
+  });
+})(jQuery);
