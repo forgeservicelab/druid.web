e0824f6fc4f06db7cc7d82aa721cb77d717b0022
Ari Ruuska <ruuska.ari@gmail.com>
2014-05-16 11:36:08 +0300

    Added last-last-last-…. class

--- sites/all/modules/features/forge_web_tutorial/forge_web_tutorial.js
+++ sites/all/modules/features/forge_web_tutorial/forge_web_tutorial.js
@@ -8,10 +8,15 @@
   Drupal.behaviors.forgeWebTotorial = {
     attach: function (context, settings) {
       var active = $("#block-menu-menu-tutorial-menu").find(".active-trail");
+      // TODO: this can do with loop and it's more elegant
       $(active).prev('li').addClass('next-from-active');
       $(active).next('li').addClass('next-from-active');
       $(active).prev('li').prev('li').addClass('last-from-active');
       $(active).next('li').next('li').addClass('last-from-active');
+      $(active).prev('li').prev('li').prev('li').addClass('last-last-from-active');
+      $(active).next('li').next('li').next('li').addClass('last-last-from-active');
+      $(active).prev('li').prev('li').prev('li').prev('li').addClass('last-last-last-from-active');
+      $(active).next('li').next('li').next('li').next('li').addClass('last-last-last-from-active');
     }
   };
 })(jQuery);
