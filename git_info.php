3945331909fe3856ae16a4df74135810cb1b1ce2
Jorge-Rodriguez <jorge.rodriguez@kampiki.com>
2014-09-18 14:48:03 +0300

    Added retry to fetch the hash if no tag is present.

--- git_info.php
+++ git_info.php
@@ -1,5 +1,9 @@
 <?php
 $tag_name = exec('git describe --tags');
+# No tag? try getting the hash.
+if(empty($tag_name)) {
+  $tag_name = substr(exec('git rev-parse HEAD'), 0, 9);
+}
 if(empty($tag_name)) {
   $tag_name = "???";
   $err_msg = "There is no current tag or git commands can't execute from php. Please check Apache error log for more information.";
