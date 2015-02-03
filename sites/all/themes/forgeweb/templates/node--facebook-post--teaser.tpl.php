048a220fba681b9061d10c1c5e4682a922cd10b6
bela.patkai <bela.patkai@gmail.com>
2015-02-03 15:35:54 +0200

    Fix Facebook link sharing

--- sites/all/themes/forgeweb/templates/node--facebook-post--teaser.tpl.php
+++ sites/all/themes/forgeweb/templates/node--facebook-post--teaser.tpl.php
@@ -86,14 +86,20 @@
     // We hide the comments and links now so that we can render them later.
     hide($content['comments']);
     hide($content['links']);
+
+    // For the moment the Facebook post's direct URL is shared
+    $share_fb_url = 'https://www.facebook.com/forgeservicelab/posts/' . $node->title;
+
+    // The shared summary text is the original one in the node object
+    $shared_text = urlencode($node->body['und'][0]['safe_value']);
+
     print render($content);
     ?>
     <div class="feed-item-type">Facebook</div>
     <div class="social-icons-wrapper">
       <span class="social-icons-toggle"><?php print t('Share'); ?></span>
       <div class="social-icons">
-        <a href="https://www.facebook.com/sharer/sharer.php?u=<?php print $share_url; ?>" class="social-icon social-icon--invert social-icon__facebook" target="_blank"><span><?php print t('Share on Facebook'); ?></span></a>
-        <a href="https://twitter.com/home?status=<?php print render($title_value); ?>%20%7C%20<?php print $share_url; ?>" class="social-icon social-icon--invert social-icon__twitter" target="_blank"><span><?php print t('Share on Twitter'); ?></span></a>
+        <a href="<?php print $share_fb_url; ?>" class="social-icon social-icon--invert social-icon__facebook" target="_blank"><span><?php print t('Share on Facebook'); ?></span></a>        <a href="https://twitter.com/home?status=<?php print render($title_value); ?>%20%7C%20<?php print $share_url; ?>" class="social-icon social-icon--invert social-icon__twitter" target="_blank"><span><?php print t('Share on Twitter'); ?></span></a>
         <a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php print $share_url; ?>&title=<?php print render($title_value); ?>&summary=&source=" class="social-icon social-icon--invert social-icon__linkedin" target="_blank"><span><?php print t('Share on LinkedIn'); ?></span></a>
       </div>
     </div>
